86a59be2817a40a977f5732e34ccdd8a155a7607
kartik8460 <umeshgupta700@gmail.com>
2020-07-14 14:28:24 +0530

    created all required files i.e components,services, etc

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private auth: AngularFireAuth) {}
+
+  signUp(email: string, password: string) {
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  signIp(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  getUser() {
+    return this.auth.authState;
+  }
+
+  signOut() {
+    return this.auth.signOut();
+  }
+}
